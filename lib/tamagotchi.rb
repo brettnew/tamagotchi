c3326d89feb474c4b94330cd5cc20c1cc5645a41
Brett New and Halle Williams <brett@>
2016-07-06 16:45:27 -0700

    adds specs and methods to to increase and decrease levels

--- lib/tamagotchi.rb
+++ lib/tamagotchi.rb
@@ -1,10 +1,11 @@
 class Tamagotchi
-
+  @@last_active = nil
   define_method(:initialize) do |my_pet_name|
     @my_pet_name=my_pet_name
-    @food_level = food_level
-    @sleep_level = sleep_level
-    @activity_level = activity_level
+    @food_level = 500
+    @sleep_level = 500
+    @activity_level = 500
+    @origin_time = Time.new
   end
 
   define_method(:name) do
@@ -12,18 +13,45 @@ class Tamagotchi
   end
 
   define_method(:food_level) do
-    @food_level = 10
+    @food_level
   end
 
   define_method(:sleep_level) do
-    @sleep_level = 10
+    @sleep_level
   end
 
   define_method(:activity_level) do
-    @activity_level = 10
+    @activity_level
   end
 
   define_method(:is_alive?) do
     (@food_level > 0) && (@sleep_level > 0) && (@activity_level > 0)
+  end
+
+  define_method(:feed) do
+    @food_level += 20
+  end
+
+  define_method(:nap) do
+    @sleep_level += 20
+  end
+
+  define_method(:excercise) do
+    @activity_level += 20
+  end
+
+  define_singleton_method(:set_last_active) do
+    @@last_active = Time.now.to_i
+  end
+
+  define_singleton_method(:time_interval) do
+      now = Time.now.to_i
+      (now - @@last_active) / 60
+  end
+
+  define_method(:time_passes) do |interval|
+    @food_level -= (2 * interval).ceil()
+    @sleep_level -= (1 *interval).ceil()
+    @activity_level -= (1 * interval).ceil()
   end
 end
--- spec/tamagotchi_spec.rb
+++ spec/tamagotchi_spec.rb
@@ -7,9 +7,9 @@ describe(Tamagotchi) do
     it("sets the name and life levels of a new Tamagotchi") do
       my_pet = Tamagotchi.new("lil dragon")
       expect(my_pet.name()).to(eq("lil dragon"))
-      expect(my_pet.food_level()).to(eq(10))
-      expect(my_pet.sleep_level()).to(eq(10))
-      expect(my_pet.activity_level()).to(eq(10))
+      expect(my_pet.food_level()).to(eq(500))
+      expect(my_pet.sleep_level()).to(eq(500))
+      expect(my_pet.activity_level()).to(eq(500))
     end
   end
 
@@ -17,6 +17,42 @@ describe(Tamagotchi) do
     it("is alive if the food level is above 0") do
       my_pet = Tamagotchi.new("lil dragon")
       expect(my_pet.is_alive?()).to(eq(true))
+    end
+  end
+
+  describe("#feed") do
+    it("adds 1 to food level") do
+      my_pet = Tamagotchi.new("lil dragon")
+      expect(my_pet.feed()).to(eq(520))
+    end
+  end
+
+  describe("#nap") do
+    it("adds 1 to sleep level") do
+      my_pet = Tamagotchi.new("lil dragon")
+      expect(my_pet.nap()).to(eq(520))
+    end
+  end
+
+  describe("#exercise") do
+    it("adds 1 to activity level") do
+      my_pet = Tamagotchi.new("lil dragon")
+      expect(my_pet.excercise()).to(eq(520))
+    end
+  end
+
+  # it("is dead if the food level is 0") do
+  #     my_pet = Tamagotchi.new("lil dragon")
+  #     my_pet.set_food_level()
+  #     expect(my_pet.is_alive?()).to(eq(false))
+  #   end
+
+
+    describe("#time_passes") do
+    it("decreases the amount of food the Tamagotchi has left by 1") do
+      my_pet = Tamagotchi.new("lil dragon")
+      my_pet.time_passes(1)  #decide what trigger you will use to make time pass
+      expect(my_pet.food_level()).to(eq(498))
     end
   end
 end
